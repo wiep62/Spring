7e672b2f87d78ae6d0a769765cecc93370f2b935
MVN home <wiep1994@yandex.ru>
2024-02-06 00:00:59 +0300

    delete @Component and all

--- src/main/java/org/example/SimpleBean.java
+++ src/main/java/org/example/SimpleBean.java
@@ -1,22 +1,16 @@
 package org.example;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 public class SimpleBean {
 public SimpleBean() {
     System.out.println("SimpleBean created");
 }
 
-@PostConstruct
 public void afterInit(){
     System.out.println("SimpleBean calling afterInit");
 }
 
-@PreDestroy
 public void beforeDestroy(){
     System.out.println("Simple Bean calling beforeDestroy");
 }
