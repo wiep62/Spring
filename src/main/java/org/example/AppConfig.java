d47ceee70fe96f2201e8b77959912203a736dff2
MVN home <wiep1994@yandex.ru>
2024-02-05 23:27:37 +0300

    delete files

--- src/main/java/org/example/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
-
-@ComponentScan("org.example")
-public class AppConfig {
-
-}
