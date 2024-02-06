75820ec47b0f0190f1ec957a19a869e96832d011
MVN home <wiep1994@yandex.ru>
2024-02-06 17:21:35 +0300

    YAML

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.springframework:spring-context:6.0.8")
 implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("org.yaml:snakeyaml:1.30")
 }
 
 tasks.test {
--- src/main/java/org/example/AppConfig.java
+++ src/main/java/org/example/AppConfig.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -10,7 +11,11 @@ public class AppConfig {
 @Bean
     public static PropertySourcesPlaceholderConfigurer properties(){
     PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
-    configurer.setLocation(new ClassPathResource("application.properties"));
+    //указываем настройки ЯМЛ
+    YamlPropertiesFactoryBean yamlPropertiesFactoryBean = new YamlPropertiesFactoryBean();
+    yamlPropertiesFactoryBean.setResources(new ClassPathResource("application.yaml"));
+   // configurer.setLocation(new ClassPathResource("application.properties"));
+    configurer.setProperties(yamlPropertiesFactoryBean.getObject());
     //вернем настроеный соурс
     return configurer;
 }
